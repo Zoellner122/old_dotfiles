27e0dab874880826b58d5510e5c4df662c98b7ad
Zoellner122 <r1997@live.nl>
2022-12-09 11:28:23 +0100

    Tweaking now playing script

--- dot_config/polybar/scripts/executable_nowplaying.sh
+++ dot_config/polybar/scripts/executable_nowplaying.sh
@@ -3,11 +3,11 @@ now_playing() {
     cider=$(playerctl -p cider status)
     if [ $spotify == "Playing" ]
         then
-            now_playing=$(playerctl -p spotify metadata --format "{{ title }} - {{ artist }}" | sed 's/\(.\{40\}\).*/\1.../')
+            now_playing=$(playerctl -p spotify metadata --format "{{ title }} - {{ artist }}" | sed 's/\(.\{25\}\).*/\1.../')
             echo "%{F#1db954}%{T1}阮 %{T-}%{F-} %{T1}$now_playing%{T-}"
     elif [ $cider == "Playing" ] 
         then
-            now_playing=$(playerctl -p cider metadata --format "{{ title }} - {{ artist }}" | sed 's/\(.\{40\}\).*/\1.../')
+            now_playing=$(playerctl -p cider metadata --format "{{ title }} - {{ artist }}" | sed 's/\(.\{25\}\).*/\1.../')
             echo "%{T1}%{F#fc3c44}%{T-}%{F-} %{T1}$now_playing%{T-}"
     else
         echo "%{T1} Nothing is playing%{T-}"
